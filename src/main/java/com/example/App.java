b6539d2295936462d82a25ffaf7bf9bf776d54fa
Karlson54 <anruban2001@gmail.com>
2023-11-24 22:43:21 +0200

    adding tests

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,92 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private int[][][] seats;
+
+    public App() {
+        seats = new int[5][10][20];
+    }
+
+    public void bookSeats(int hallNumber, int row, int[] seatsToBook) {
+        for (int seat : seatsToBook) {
+            int updatedHallNumber = hallNumber - 1;
+            int updatedRow = row - 1;
+            int updatedSeat = seat - 1;
+
+            if (updatedRow >= 0 && updatedRow < seats[updatedHallNumber].length
+                    && updatedSeat >= 0 && updatedSeat < seats[updatedHallNumber][updatedRow].length
+                    && seats[updatedHallNumber][updatedRow][updatedSeat] == 0) {
+
+                seats[updatedHallNumber][updatedRow][updatedSeat] = 1;
+                System.out.println(
+                        "Seat " + seat + " in row " + row + " in hall " + hallNumber + " booked.");
+            } else {
+                System.out.println("Seat " + seat + " in row " + row + " in hall " + hallNumber
+                        + " cannot be booked.");
+            }
+        }
+    }
+
+    public void cancelBooking(int hallNumber, int row, int[] seatsToCancel) {
+        for (int seat : seatsToCancel) {
+            int updatedHallNumber = hallNumber - 1;
+            int updatedRow = row - 1;
+            int updatedSeat = seat - 1;
+
+            if (updatedRow >= 0 && updatedRow < seats[updatedHallNumber].length
+                    && updatedSeat >= 0 && updatedSeat < seats[updatedHallNumber][updatedRow].length
+                    && seats[updatedHallNumber][updatedRow][updatedSeat] == 1) {
+
+                seats[updatedHallNumber][updatedRow][updatedSeat] = 0;
+                System.out.println(
+                        "Cancellation of seat " + seat + " in row " + row + " in hall " + hallNumber + " successful.");
+            } else {
+                System.out.println(
+                        "Seat " + seat + " in row " + row + " in hall " + hallNumber + " was not booked.");
+            }
+        }
+    }
+
+    public boolean checkAvailability(int hallNumber, int numSeats) {
+        int updatedHallNumber = hallNumber - 1;
+        for (int row = 0; row < seats[updatedHallNumber].length; row++) {
+            for (int seat = 0; seat <= seats[updatedHallNumber][row].length - numSeats; seat++) {
+                boolean available = true;
+                for (int i = 0; i < numSeats; i++) {
+                    if (seats[updatedHallNumber][row][seat + i] == 1) {
+                        available = false;
+                        break;
+                    }
+                }
+                if (available) {
+                    System.out.println(
+                            numSeats + " seats available in row " + (row + 1) + " in hall " + hallNumber + ".");
+                    return true;
+                }
+            }
+        }
+        System.out.println(numSeats + " seats not available in hall " + hallNumber + ".");
+        return false;
+    }
+
+    public void printhall(int hallNumber) {
+        int updatedHallNumber = hallNumber - 1;
+        System.out.println("Seating arrangement in hall " + hallNumber + ":");
+        for (int row = 0; row < seats[updatedHallNumber].length; row++) {
+            System.out.print("Row " + (row + 1) + ": ");
+            for (int seat = 0; seat < seats[updatedHallNumber][row].length; seat++) {
+                System.out.print(seats[updatedHallNumber][row][seat] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        App cinema = new App();
+        cinema.bookSeats(1, 1, new int[] { 4, 5, 6 });
+        cinema.printhall(1);
+        cinema.checkAvailability(1, 18);
+        cinema.cancelBooking(1, 3, new int[] { 5, 6 });
+        cinema.printhall(1);
     }
 }
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,20 +1,49 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
+    private App cinema;
+
+    @Before
+    public void setUp() {
+        cinema = new App();
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testBookSeats() {
+        cinema.bookSeats(1, 1, new int[]{1, 2, 3});
+        assertTrue(cinema.checkAvailability(1, 7));
+    }
+
+    @Test
+    public void testCancelBooking() {
+        cinema.bookSeats(2, 2, new int[]{4, 5, 6});
+        cinema.cancelBooking(2, 2, new int[]{5, 6});
+        cinema.printhall(2);
+        assertTrue(cinema.checkAvailability(2, 3));
+    }
+
+    @Test
+    public void testCheckAvailability() {
+        cinema.bookSeats(3, 1, new int[]{8, 9, 10,});
+        cinema.bookSeats(3, 2, new int[]{8, 9, 10,});
+        cinema.bookSeats(3, 3, new int[]{8, 9, 10,});
+        cinema.bookSeats(3, 4, new int[]{8, 9, 10,});
+        cinema.bookSeats(3, 5, new int[]{8, 9, 10,});
+        cinema.bookSeats(3, 6, new int[]{8, 9, 10,});
+        cinema.bookSeats(3, 7, new int[]{8, 9, 10,});
+        cinema.bookSeats(3, 8, new int[]{8, 9, 10,});
+        cinema.bookSeats(3, 9, new int[]{8, 9, 10,});
+        cinema.bookSeats(3, 10, new int[]{8, 9, 10,});
+        assertFalse(cinema.checkAvailability(3, 18));
+    }
+
+    @Test
+    public void testPrintHall() {
+        cinema.printhall(4);
     }
 }
